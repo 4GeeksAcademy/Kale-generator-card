855b0eeb11176dc7d6faf37419e9df18a8823168
KaleCordero <kalecascante61@gmail.com>
2024-09-10 03:36:08 +0000

    baraja

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let button = document.querySelector("#generateButton");
+  button.addEventListener("click", generateCard);
+  generateCard();
 };
+function generateCard() {
+  let generateFiguras = Math.floor(Math.random() * 4);
+  let arrFiguras = ["♦", "♥", "♠", "♣"];
+  let colorFiguras = generateFiguras < 2 ? "red" : "black";
+  generateFiguras = arrFiguras[generateFiguras];
+  function generateNumber() {
+    let number = Math.floor(Math.random() * 13) + 1;
+    switch (number) {
+      case 11:
+        number = "J";
+        break;
+      case 12:
+        number = "Q";
+        break;
+      case 13:
+        number = "K";
+        break;
+      case 1:
+        number = "A";
+        break;
+    }
+    return number;
+  }
+  let valor = generateNumber();
+
+  document.getElementById("generateNumber").innerText = valor;
+  let figuras = document.querySelectorAll(".generateFiguras");
+  figuras.forEach(fig => {
+    fig.innerText = generateFiguras;
+    fig.style.color = colorFiguras;
+  });
+}
+setInterval(generateCard, 10000);
